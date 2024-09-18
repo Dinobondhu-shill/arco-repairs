c29b1bb6b167894551bec2ebb5e1bd0a578cffa2
Dinobondhu-shill <dinu.webdev@gmail.com>
2024-09-19 02:41:58 +0600

    navbar added

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline"> 
+    <h1 className=" font-bold underline"> 
     MY FIRST APP 
 </h1> 
   );
--- src/public/components/Navbar.jsx
+++ src/public/components/Navbar.jsx
@@ -1,15 +1,74 @@
 import Image from 'next/image';
 import React from 'react';
 import img from '@/public/images/image 4.png'
+import Link from 'next/link';
+import { FaFacebook, FaLinkedin  } from "react-icons/fa";
+import { FaXTwitter, FaInstagram  } from "react-icons/fa6";
+import { GrMapLocation } from "react-icons/gr";
 
+
+const navbar = [
+  
+  {
+    route:"Home",
+    path:"/"
+  },
+  {
+    route:"Services",
+    path:"/services"
+  },
+  {
+    route:"About Us",
+    path:"/about"
+  },
+  {
+    route:"Contact",
+    path:"/contact"
+  },
+  {
+    route:"Login",
+    path:"/login"
+  },
+  {
+    route:"Sign Up",
+    path:"/signup"
+  },
+  
+]
 const Navbar = () => {
   return (
-    <div className='h-14 flex '>
-      <div>
-        <Image src={img} className='mx-6'></Image>
+    <div className='h-[150px] flex '>
+      <div className='mx-6 flex justify-center items-center'>
+        <Image src={img} ></Image>
       </div>
-      <div className='bg-[#FABB05] w-full h-1/2 flex items-end'>
-    this is upper head
+      <div className=' w-full'>
+      <div className='bg-[#FABB05] h-1/3 flex items-center justify-between  pr-6'>
+    <p className='font-semibold px-6 flex items-center gap-4'> <GrMapLocation className='text-lg'/>
+     Serving Washington, California & Surrounding Areas</p>
+    <div>
+    <ul className='flex text-xl gap-3'>
+      <li><FaFacebook /></li>
+      <li><FaLinkedin /></li>
+      <li><FaXTwitter /></li>
+      <li><FaInstagram /></li>
+    </ul>
+    </div>
+      </div>
+      <div className='h-2/3 flex items-center justify-between'>
+        <div>
+        {
+          navbar.map(nav=> <Link key={nav.route} href={nav?.path} className='pr-4 hover:text-[#FABB05] duration-300'>{nav.route}</Link>)
+        }
+        </div>
+        <div className='flex'>
+          <Link href={'/'} className='py-2 px-4 bg-[#FABB05] font-semibold flex items-center rounded-full'>REQUEST AN APPOINTMENT</Link>
+          <div className='px-6'>
+        <h5>Call Today: 
+        </h5>
+        <h4 className='text-3xl '>(321) 341-9833</h4>
+          </div>
+        </div>
+      </div>
       </div>
     </div>
   );
